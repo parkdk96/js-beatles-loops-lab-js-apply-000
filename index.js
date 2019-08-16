e18990f98905c49eb79538389bb874f62eab828b
Daniel Park <parkdk96@gmail.com>
2019-08-16 06:52:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var theBand = [];
+  var whatTheyPlay = ""
+  for (var i = 0; i < musicians.length; i++) {
+    whatTheyPlay = musicians[i] + " plays " + instruments[i];
+    theBand.push(whatTheyPlay)
+  }
+  return theBand;
+}
+
+
+function johnLennonFacts(facts) {
+  var surprisingFacts = [];
+  var theFacts = "";
+  var i = 0;
+  while (i < facts.length) {
+    theFacts = facts[i] + "!!!";
+    surprisingFacts.push(theFacts)
+  }
+  return surprisingFacts;
+}
+
+function iLoveTheBeatles(num) {
+  var myArray = [];
+  do {
+    myArray.push("I love the Beatles!");
+    num++
+  } while (num < 15)
+}
